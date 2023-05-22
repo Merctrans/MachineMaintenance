543856989bed5a39de0cf3fb1799c62537a17b7d
Truong Tang <txt2408@gmail.com>
2023-05-22 11:22:11 +0700

    add views

--- local-addons/MachineMaintenance/__manifest__.py
+++ local-addons/MachineMaintenance/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Machine Maintenance",
+    'name': "Machine Maintenance Checksheet",
 
     'summary': """
         Hoya Machine Maintenance Module""",
@@ -19,13 +19,13 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'maintenance' ],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/check_sheet.xml',
+        'views/machine.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
--- local-addons/MachineMaintenance/models/checksheet.py
+++ local-addons/MachineMaintenance/models/checksheet.py
@@ -2,38 +2,6 @@
 from odoo import fields, api, models
 
 
-class Department(models.Model):
-    _name = "department"
-    _description = "Department in the company"
-    _rec_name = "code"
-
-    name = fields.Char('Name', required=True)
-    code = fields.Char('Department Code', required=True)
-    active = fields.Boolean('Active', default=True)
-
-    @api.onchange('code')
-    def _onchange_code(self):
-        """Code field to be capitalized"""
-        new_code = self.code
-        self.code = new_code.upper()
-
-
-class Factory(models.Model):
-    _name = "factory"
-    _description = "Factory model"
-    _rec_name = "code"
-
-    name = fields.Char('Name', required=True)
-    code = fields.Char('Factory Code', required=True)
-    active = fields.Boolean('Active', default=True)
-
-    @api.onchange('code')
-    def _onchange_code(self):
-        """Code field to be capitalized"""
-        new_code = self.code
-        self.code = new_code.upper()
-
-
 class CheckSheet(models.Model):
     _name = "check.sheet"
     _description = "Check Sheet Template"
@@ -51,7 +19,8 @@ class CheckSheet(models.Model):
     factory = fields.Many2one('factory', string='Factory*', required=True)
     created_by = fields.Many2one('res.users', string='Created By*', required=True)
     create_date = fields.Datetime(readonly=True)
-    increment_number = fields.Integer(string='Number Incremented', default=lambda self: self.env['ir.sequence'].next_by_code('increment_your_field'))
+    increment_number = fields.Integer(string='Number Incremented',
+                                      default=lambda self: self.env['ir.sequence'].next_by_code('increment_your_field'))
 
     @api.onchange('department')
     def _generate_check_sheet_code(self):
@@ -79,3 +48,76 @@ class CheckSheet(models.Model):
                     rec.frequency = 360
                 else:
                     rec.frequency = 0
+
+    entry_data = fields.One2many('entry.data', 'check_sheet', string="Entry Data")
+    device = fields.Many2many('maintenance.device', string='Device')
+
+
+class EntryData(models.Model):
+    _name = 'entry.data'
+    _description = 'Data Entry for Check sheet'
+
+    check_sheet = fields.Many2one('check.sheet', string='Check Sheet')
+    work_detail = fields.Char('Detailed Work', required=True)
+    action = fields.Selection(selection=[("check", "Check"),
+                                         ("check_and_replace", "Check/Replace"),
+                                         ("check_and_adjust", "Check/Adjust")], string='Action', required=True,
+                              default='check')
+    entry_type = fields.Selection(selection=[("number", "number"),
+                                             ("text", "Text")])
+
+    lcl = fields.Float(string='LCL', default=0.0)
+    ucl = fields.Float(string='UCL', default=0.0)
+    value_show = fields.Char(string='Value Show')
+    result_check = fields.Selection(selection=[('ok', 'OK'), ('ng', 'NG')], default='ok',
+                                    compute='_auto_judgement', inverse='_inverse_compute')
+
+    """Auto Judgement"""
+
+    def is_float(element: any) -> bool:
+        # If you expect None to be passed:
+        if element is None:
+            return False
+        try:
+            float(element)
+            return True
+        except ValueError:
+            return False
+
+    @api.depends('ucl', 'lcl')
+    @api.onchange('ucl', 'lcl', 'value_show')
+    def _auto_judgement(self):
+        for rec in self:
+            if self.is_float(self.value_show):
+                if float(self.value_show) < self.lcl or float(self.value_show) > self.ucl:
+                    self.update({'result_check': 'ng'})
+                else:
+                    self.update({'result_check': 'ok'})
+            else:
+                raise ValueError("Value is not float, cannot use auto judgement")
+
+    def _inverse_compute(self):
+        return
+
+    action_ng = fields.Char(string='Action when NG')
+    value_show_after_action = fields.Char(string='Value Showed After Action')
+    result_check_after_action = fields.Selection(selection=[('ok', 'OK'), ('ng', 'NG')], default='ok',
+                                                 compute='_auto_judgement_after_action',
+                                                 inverse='_inverse_compute_after_action')
+
+    """Auto Judgement after action"""
+
+    @api.depends('ucl', 'lcl')
+    @api.onchange('ucl', 'lcl', 'value_show_after_action')
+    def _auto_judgement_after_action(self):
+        for rec in self:
+            if self.is_float(self.value_show_after_action):
+                if float(self.value_show_after_action) < self.lcl or float(self.value_show_after_action) > self.ucl:
+                    self.update({'result_check': 'ng'})
+                else:
+                    self.update({'result_check': 'ok'})
+            else:
+                raise ValueError("Value is not float, cannot use auto judgement")
+
+    def _inverse_compute_after_action(self):
+        return
